5d62bcb2ba8e2f7b1654a62ad04454c14c00d990
yumyum <yumyum7@gmail.com>
2011-04-25 02:33:32 +0900

    user db enabled
    each rss entry reflesh in only (re-)add stuation of that entry.

--- src/yumyumapp.py
+++ src/yumyumapp.py
@@ -6,24 +6,31 @@ from xml.etree import ElementTree
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
+from google.appengine.api import users
 import os
 from google.appengine.ext.webapp import template
 
-class Greeting(db.Model):
-	author = db.UserProperty()
-	content = db.StringProperty(multiline=True)
+class UserData(db.Model):
+	user = db.UserProperty(required=True,auto_current_user_add=True)
+	url = db.LinkProperty(required=True)
+	ref = db.ReferenceProperty()
 	date = db.DateTimeProperty(auto_now_add=True)
+
+class SiteData(db.Model):
+	url = db.LinkProperty(required=True)
+	data = db.BlobProperty()
 
 class MainPage(webapp.RequestHandler):
 	def get(self):
-		path = os.path.join(os.path.dirname(__file__), 'index.html')
-		self.response.out.write(template.render(path, None))
-
-class GetRSS(webapp.RequestHandler):
-	def post(self):
-		result = urlfetch.fetch(self.request.get('url'))
-		if result.status_code == 200:
-			xml = ElementTree.fromstring(result.content)
+		user = users.get_current_user()
+		if not user:
+			self.redirect(users.create_login_url(self.request.path))
+		query = UserData.all()
+		query.filter('user =', user)
+		results = query.fetch(10)
+		for result in results:
+			siteData = result.ref
+			xml = ElementTree.fromstring(siteData.data)
 			channel = xml.find("channel")
 			self.response.out.write("<p>%s</p>" % channel.find("title").text)
 			items = channel.findall("item")
@@ -34,6 +41,37 @@ class GetRSS(webapp.RequestHandler):
 				if description != None:
 					self.response.out.write("Content : %s</p>" % description.text)
 				i += 1
+		path = os.path.join(os.path.dirname(__file__), 'index.html')
+		self.response.out.write(template.render(path, None))
+
+class AddSite(webapp.RequestHandler):
+	def post(self):
+		targetURL = self.request.get('url')
+		result = urlfetch.fetch(targetURL)
+		if result.status_code == 200:
+			user = users.get_current_user()
+			userDataKey = user.nickname()+targetURL
+			userData = UserData.get_or_insert(userDataKey, url=targetURL)
+			siteData = SiteData.get_by_key_name(targetURL)
+			if not siteData:
+				siteData = SiteData.get_or_insert(targetURL, url=targetURL)
+			siteData.data = result.content
+			siteData.put()
+			userData.ref = siteData
+			userData.put()
+			self.redirect('/')
+#		if result.status_code == 200:
+#			xml = ElementTree.fromstring(result.content)
+#			channel = xml.find("channel")
+#			self.response.out.write("<p>%s</p>" % channel.find("title").text)
+#			items = channel.findall("item")
+#			i = 1
+#			for item in items:
+#				self.response.out.write("<p>Item%d : <a href=\"%s\">%s</a><br>" % (i, item.find("link").text, item.find("title").text))
+#				description = item.find("description")
+#				if description != None:
+#					self.response.out.write("Content : %s</p>" % description.text)
+#				i += 1
 				
 					
 #			title = xml.find("title")
@@ -46,7 +84,7 @@ class GetRSS(webapp.RequestHandler):
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
-                                      ('/rss', GetRSS)],
+                                      ('/rss', AddSite)],
                                      debug=True)
 
 def main():
